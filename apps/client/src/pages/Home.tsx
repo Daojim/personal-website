c2e9fa6b10d740c6152606903fb2b50029c80ede
Daojim <jimmyddao@gmail.com>
2025-08-18 01:06:26 -0400

    content: add blog topics section and navigation to home page

--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <>
-      <h1>What is this?</h1>
+      <h2>What is this?</h2>
       <p>
         Welcome to my personal website. I'm going to use this website to blog
         about my experiences and thoughts.
+      </p>
+      <h2>What am I going to write about?</h2>
+      <p>
+        I plan to write about a variety of topics, such as the technologies I'm
+        learning, video games I'm playing or have played, and my personal life.
+      </p>
+      <p>
+        I will be posting more at <Link to={{ pathname: "/blog" }}>/blog</Link>
       </p>
     </>
   );
