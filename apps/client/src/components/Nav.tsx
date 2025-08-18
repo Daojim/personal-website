839dd8d5afe199d8506a6843eb6354a7af904a93
Daojim <jimmyddao@gmail.com>
2025-08-18 17:44:46 -0400

    Change CSS for site layout and navigation bar

--- apps/client/src/components/Nav.tsx
+++ apps/client/src/components/Nav.tsx
@@ -5,8 +5,18 @@ function Nav() {
     <>
       <nav>
         <div className="container">
-          <NavLink to="/">Home</NavLink> <NavLink to="/blog">Blog</NavLink>{" "}
-          <NavLink to="/about">About</NavLink>
+          <div className="internalNav">
+            <NavLink to="/">Home</NavLink> <NavLink to="/blog">Blog</NavLink>{" "}
+            <NavLink to="/about">About</NavLink>
+          </div>
+          <div className="externalNav">
+            <NavLink to="https://github.com/daojim" target="_blank">
+              GitHub
+            </NavLink>
+            <NavLink to="https://www.linkedin.com/in/daojim/" target="_blank">
+              LinkedIn
+            </NavLink>
+          </div>
         </div>
       </nav>
     </>
