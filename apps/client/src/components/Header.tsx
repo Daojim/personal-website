8b29febbc5c893cbc2617a341dd0196878311119
Daojim <jimmyddao@gmail.com>
2025-08-18 01:15:00 -0400

    Add header tag in Header.tsx

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,8 +1,10 @@
 function Header() {
   return (
     <>
-      <h1>Jimmy Dao</h1>
-      <h2>Software Developer Student</h2>
+      <header>
+        <h1>Jimmy Dao</h1>
+        <h2>Software Developer Student</h2>
+      </header>
     </>
   );
 }
