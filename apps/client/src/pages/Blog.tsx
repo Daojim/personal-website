ca840a1cb5033028f37bbdc119622ec8daa25aa6
Daojim <jimmyddao@gmail.com>
2025-09-18 20:50:05 -0400

    Wire posts.ts to Blog.tsx

--- apps/client/src/pages/Blog.tsx
+++ apps/client/src/pages/Blog.tsx
@@ -1,8 +1,33 @@
+import { posts } from "../content/posts";
+
 function Blog() {
   return (
     <>
       <h2>My Blog</h2>
-      <p>I'll be posting a list of my blogs here</p>
+      {posts.length > 0 ? (
+        <div className="blog-posts">
+          {posts.map((post) => (
+            <article key={post.slug}>
+              <h3>{post.title}</h3>
+              <time dateTime={post.date}>
+                {new Date(post.date).toLocaleDateString()}
+              </time>
+              {post.excerpt && <p>{post.excerpt}</p>}
+              {post.tags && (
+                <div className="tags">
+                  {post.tags.map((tag) => (
+                    <span key={tag} className="tag">
+                      #{tag}{" "}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </article>
+          ))}
+        </div>
+      ) : (
+        <p>No blog posts yet. Check back soon!</p>
+      )}
     </>
   );
 }
